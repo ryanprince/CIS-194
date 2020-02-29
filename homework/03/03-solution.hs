731417d87254872dbfd0c242d10d9234f51d6df5
Ryan Prince <rnprince7@gmail.com>
2020-02-29 13:20:20 -0500

    Shorten Exercises 1 and 3.

--- homework/03/03-solution.hs
+++ homework/03/03-solution.hs
@@ -1,11 +1,11 @@
 {-# OPTIONS_GHC -Wall #-}
 
 -- Exercise 1: Hopscotch
-_s :: [a] -> Int -> [a]
-_s v n = map (v!!) [n-1,2*n-1..length v - 1]
+s :: [a] -> Int -> [a]
+s v n = map (v!!) [n-1,2*n-1..length v - 1]
 
 skips :: [a] -> [[a]]
-skips v = map (_s v) [1..length v]
+skips v = map (s v) [1..length v]
 
 -- Exercise 2: Local maxima
 localMaxima :: [Integer] -> [Integer]
@@ -16,12 +16,13 @@ localMaxima _ = []
 
 -- Exercise 3: Histogram
 histogram :: [Integer] -> String
-histogram v = _h $ map (\i -> fromIntegral (length (filter (==i) v))) [0..9]
-_h :: [Integer] -> String
-_h v = let m = maximum v
-           l = map (\x -> if x == m then '*' else ' ') v
-           w = map (\x -> if x == m then x - 1 else x) v
-       in '\n' : if m == 0 then "==========\n0123456789\n" else l ++ _h w
+histogram v = q $ map (\i -> fromIntegral (length (filter (==i) v))) [0..9]
+q :: [Integer] -> String
+q v = let m       = maximum v
+          z y n x = if x == m then y else n
+          l       = map (z '*' ' ') v
+          w       = map (\x -> z (x - 1) x x) v
+      in '\n' : if m == 0 then "==========\n0123456789\n" else l ++ q w
 
 -- a = skips "ABCD"
 -- b = skips "hello!"
